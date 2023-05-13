90641fd0b7178aefade09cf43860c8c156143de0
faizafrin <faizasworld2022@gmail.com>
2023-05-13 23:24:09 +0530

    firstcommit

---
+++ common/dbconfig.js
@@ -0,0 +1,1 @@
+const dbUrl = 'mongodb+srv://faizaafrin:Todaywin10@cluster0.yshpzyi.mongodb.net'
---
+++ schemas/userSchema.js
@@ -0,0 +1,29 @@
+const validator = require('validator');
+const  mongoose  = require('mongoose');
+
+var UserSchema = new mongoose.Schema(
+  {
+    name:{type:String,required:true},
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        validate:(value)=>{
+            return validator.isEmail(value)   
+        }
+
+    },
+    mobile:{type:String,default:'000-000-0000'},
+    password:{type:String,required:true},
+    role:{type:String,default:'user'},
+    createAt:{type:Date,default:Date.now}
+  },
+  {
+    collection:'users',  
+    versionKey:false
+  }
+
+)
+
+let UserModel = mongoose.model('user',UserSchema)
+module.exports={UserModel} 
